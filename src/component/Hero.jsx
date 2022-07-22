5dafe8e703c6b4c0b9d5fe95c9a24b114e257d67
ypushpa12 <pushpalatha.yarra@gspann.com>
2022-07-22 16:34:14 +0530

    changes

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "../Style/Her.scss"
 // import maxwidth from "../Images/max-width_header.jpg"
-import  shopping from "../Images/cart.png"
+import shopping from "../Images/cart.png"
 
 function Hero() {
   return (
@@ -22,8 +22,9 @@ function Hero() {
               </div>
               <div class="aem-GridColumn aem-GridColumn--default--8">
                 <div class="card-img">
-                  <img src="https://img.freepik.com/free-photo/shopping-bag-cart_23-2148879372.jpg?w=2000" class="card-img-top" alt="" />
-               {/* <img src={shopping}/> */}
+                  {/* <img src="https://images.unsplash.com/photo-1590639815345-f30dd48aba1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJhbCUyMGJlYXV0eXxlbnwwfHwwfHw%3D&w=1000&q=80" class="card-img-top" alt="" /> */}
+                <img src="https://img.freepik.com/free-photo/shopping-bag-cart_23-2148879372.jpg?w=2000"class="card-img-top" alt="" />
+                  {/* <img src={shopping}/> */}
                 </div>
               </div>
             </div>
@@ -35,6 +36,7 @@ function Hero() {
         <div class="aem-Grid aem-Grid--12">
           <div class="aem-GridColumn aem-GridColumn--default--8 aem-GridColumn--phone--12">
             <div class="card-img">
+               {/* <img src="https://images.unsplash.com/photo-1590639815345-f30dd48aba1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJhbCUyMGJlYXV0eXxlbnwwfHwwfHw%3D&w=1000&q=80" class="card-img-top" alt="" /> */}
               <img src="https://img.freepik.com/free-photo/shopping-bag-cart_23-2148879372.jpg?w=2000" class="card-img-top" alt="" />
               {/* <img src={shopping}/> */}
             </div>
